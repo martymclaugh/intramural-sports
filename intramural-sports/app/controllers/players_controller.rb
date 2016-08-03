2aabc6ec87a5efcb5c127aaf36f4994adf1580b1
raj7desai <raj7desai@gmail.com>
2016-08-03 16:45:28 -0700

    Implemented Login across and related methods/changes

--- intramural-sports/app/controllers/players_controller.rb
+++ intramural-sports/app/controllers/players_controller.rb
@@ -1,5 +1,20 @@
 class PlayersController < ApplicationController
+  def new
+  end
+
   def create
-    'Hello World!'
+    player = Player.new(player_params)
+    if player.save
+      session[:player_id] = player.id
+      redirect_to '/'
+    else
+      redirect_to '/signup'
+    end
+  end
+
+  private
+
+  def player_params
+    params.require(:player).permit(:name, :email, :team, :password_confirmation, :phone, :coach)
   end
 end
--- intramural-sports/config/routes.rb
+++ intramural-sports/config/routes.rb
@@ -1,10 +1,18 @@
 Rails.application.routes.draw do
-  resources :sessions
+  resources :sessions, only: [:new, :create, :destroy]
   resources :leagues do
     resources :games
     resources :teams do
       resources :players
     end
   end
+
+  get '/login' => 'sessions#new'
+  post '/login' => 'sessions#create'
+  get '/logout' => 'sessions#destroy'
+
+  get '/signup' => 'players#new'
+  post '/users' => 'players#create'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
