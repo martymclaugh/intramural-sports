58a4fda66bf27ac40ac5a7bf965b509829343027
Simon Nim <simonnim@Simons-MacBook-Air.local>
2016-08-03 16:32:31 -0700

    Add games controller and views.

--- intramural-sports/app/controllers/games_controller.rb
+++ intramural-sports/app/controllers/games_controller.rb
@@ -1,2 +1,24 @@
 class GamesController < ApplicationController
+
+def index
+
+	@games = Game.find_by(:id)
+
+end
+
+
+def show
+
+
+end
+
+
+def edit
+
+	@game = Game.find_by(:id)
+
+end
+
+
+
 end
