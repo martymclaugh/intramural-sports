6c3ee36dcc821ebd576074dfe3b55734ae733784
Marty McLaughlin <martymclaugh@gmail.com>
2016-08-03 16:21:21 -0700

    create show page for teams of a league and players of a team

--- intramural-sports/app/controllers/teams_controller.rb
+++ intramural-sports/app/controllers/teams_controller.rb
@@ -1,2 +1,9 @@
 class TeamsController < ApplicationController
+  def index
+    @teams = Team.where(league_id: params[:league_id])
+    @league = League.find(params[:league_id])
+  end
+  def show
+    @team = Team.find(params[:id])
+  end
 end
