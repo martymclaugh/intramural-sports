c1851dc9e7fd55cf968be0a28e7098e4984d25c0
Simon Nim <simonnim@Simons-MacBook-Air.local>
2016-08-04 15:29:22 -0700

    fix creating players/coaches

--- intramural-sports/app/controllers/games_controller.rb
+++ intramural-sports/app/controllers/games_controller.rb
@@ -1,25 +1,38 @@
 class GamesController < ApplicationController
 
-def index
+	def index
 
-	@games = Game.all
+		@games = Game.all
+
+	end
+
+
+	def show
+		@game = Game.find(params[:id])
+		@home_team = Team.find(@game.home_id)
+		@away_team = Team.find(@game.away_id)
+		@league = League.find(Team.find(@game.away_id).league_id)
+	end
+
+
+	def edit
+
+
+	end
+
+
+	def new
+
+	end
+
+	def create
+
+	end
 
 end
 
+protected
 
-def show
-	@game = Game.find(params[:id])
-	@home_team = Team.find(@game.home_id)
-	@away_team = Team.find(@game.away_id)
-	@league = League.find(Team.find(@game.away_id).league_id)
-end
-
-
-def edit
-
-
-end
-
-
-
+def game_params
+	params.require(:team).permit(:home_team, :away_team, :address, :date)
 end
