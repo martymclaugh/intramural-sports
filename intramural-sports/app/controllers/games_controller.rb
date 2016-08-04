cbec5aa146b4afafd8750de32f6280289359a2ba
Marty McLaughlin <martymclaugh@gmail.com>
2016-08-04 13:42:33 -0700

    add map and start on form for game page

--- intramural-sports/app/controllers/games_controller.rb
+++ intramural-sports/app/controllers/games_controller.rb
@@ -8,9 +8,10 @@ end
 
 
 def show
-
 	@game = Game.find(params[:id])
-
+	@home_team = Team.find(@game.home_id)
+	@away_team = Team.find(@game.away_id)
+	@league = League.find(Team.find(@game.away_id).league_id)
 end
 
 
