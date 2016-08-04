5a67364f2b90360d92d624dd2c178a5659ae795a
Marty McLaughlin <martymclaugh@gmail.com>
2016-08-04 14:30:17 -0700

    add update form for game scores

--- intramural-sports/app/controllers/games_controller.rb
+++ intramural-sports/app/controllers/games_controller.rb
@@ -15,11 +15,11 @@ def show
 end
 
 
-def edit
-
-
+def update
+	@game = Game.find(params[:id])
+	@league = League.find(Team.find(Game.find(params[:id]).away_id).league_id)
+	if @game.update_attributes(home_score: params[:home_score].to_i, away_score: params[:away_score].to_i)
+		redirect_to "/leagues/#{@league.id}/games/#{@game.id}"
+	end
 end
-
-
-
 end
