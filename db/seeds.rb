eff830b7339395b8b98bdf97a1f9764da0870a4a
Marty McLaughlin <martymclaugh@gmail.com>
2016-08-05 13:51:06 -0700

    changed seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,7 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-100.times do
+300.times do
   Player.create(name: Faker::Name.name, phone: Faker::PhoneNumber.phone_number, email: Faker::Internet.email, coach: false, password: '123')
 end
 
@@ -15,20 +15,20 @@ League.create(sport: "Basketball")
 League.create(sport: "Hockey")
 
 
-10.times do
+20.times do
   Team.create(name: Faker::Pokemon.name, league_id: rand(1..3))
 end
 
-20.times do
+60.times do
   Game.create(location: Faker::Address.street_address, date: DateTime.now, home_score:0, away_score:0)
 end
 
-20.times do
-  TeamGame.create(home_id: rand(1..10), away_id: rand(1..10), game_id: rand(1..20))
+60.times do
+  TeamGame.create(home_id: rand(1..20), away_id: rand(1..20), game_id: rand(1..60))
 end
 
-100.times do
-  PlayerTeam.create(team_id: rand(1..10), player_id: rand(1..100))
+300.times do
+  PlayerTeam.create(team_id: rand(1..20), player_id: rand(1..300))
 end
 
 # Game.create(home_score: rand(1..20), away_score: rand(1..20), location: Faker::Address.street_address, date: DateTime.now, home_id: rand(1..10), away_id: rand(1..10))
