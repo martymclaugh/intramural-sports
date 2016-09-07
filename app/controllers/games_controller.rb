4dccdff9b2516cb5410ff28676cb3695e92074c6
Erica Y <email": null>
2016-09-06 20:26:50 -0700

    Update logic in #new and #create for games

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -19,17 +19,15 @@ class GamesController < ApplicationController
       if params[:game][:home_team]
         @home_team = Team.find_by(name: params[:game][:home_team])
         if @home_team != @team
-          response = [@team.name] + Array.wrap(params[:game].keys)
           respond_to do |format|
-            format.json { render json: response}
+            format.json { render json: [@team.name] }
           end
         end
       elsif params[:game][:away_team]
         @away_team = Team.find_by(name: params[:game][:away_team])
         if @away_team != @team
-          response = [@team.name] + Array.wrap(params[:game].keys)
           respond_to do |format|
-            format.json { render json: response }
+            format.json { render json: [@team.name] }
           end
         end
       end
@@ -37,34 +35,17 @@ class GamesController < ApplicationController
   end
 
   def create
-    # @team = Team.find_by(id: PlayerTeam.find_by(player_id: current_user.id).team_id)
-    if request.xhr?
-      @game = Game.create(location: params[:game][:address], date: DateTime.now, home_score: 0, away_score: 0)
-      @team_game = TeamGame.create(away_id: away_team.id, home_id: home_team.id, game_id: @game.id)
-      @league = League.find(home_team.league_id)
-        if @game.valid?
-          send_text_message(home_team, away_team, @game.location, @game.date)
-          redirect_to "/leagues/#{@league.id}/games/#{@game.id}"
-        else
-          redirect_to "/leagues/#{@league.id}/teams/#{home_team.id}"
-        end
-    else
-      # home_team = Team.find_by(name: params[:game][:home_team])
-      # away_team = Team.find_by(name: params[:game][:away_team])
-      # if (home_team != away_team) && (home_team == @team || away_team == @team)
-      #   @game = Game.create(location: params[:game][:address], date: DateTime.now, home_score: 0, away_score: 0)
-      #   @team_game = TeamGame.create(away_id: away_team.id, home_id: home_team.id, game_id: @game.id)
-      #   @league = League.find(home_team.league_id)
-      #     if @game.valid?
-      #       send_text_message(home_team, away_team, @game.location, @game.date)
-      #       redirect_to "/leagues/#{@league.id}/games/#{@game.id}"
-      #     else
-      #       redirect_to "/leagues/#{@league.id}/teams/#{home_team.id}"
-      #     end
-      # else
-      #  @error = "Please choose two different teams"
-      # end
-    end
+    home_team = Team.find_by(name: params[:game][:home_team])
+    away_team = Team.find_by(name: params[:game][:away_team])
+    @game = Game.create(location: params[:game][:address], date: DateTime.now, home_score: 0, away_score: 0)
+    @team_game = TeamGame.create(away_id: away_team.id, home_id: home_team.id, game_id: @game.id)
+    @league = League.find(home_team.league_id)
+      if @game.valid?
+        send_text_message(home_team, away_team, @game.location, @game.date)
+        redirect_to "/leagues/#{@league.id}/games/#{@game.id}"
+      else
+        redirect_to "/leagues/#{@league.id}/teams/#{home_team.id}"
+      end
   end
 
   def update
@@ -88,13 +69,15 @@ class GamesController < ApplicationController
     twilio_token = ENV['TWILIO_AUTH_TOKEN']
     twilio_phone_number = ENV['TWILIO_NUMBER']
 
-    @twilio_client = Twilio::REST::Client.new(twilio_sid, twilio_token)
-    numbers_to_send_to.each do |number|
-      @twilio_client.account.sms.messages.create(
-        :from => twilio_phone_number,
-        :to => number,
-        :body => twilio_body
-        )
-    end
+    #UNCOMMENT WHEN READY TO USE
+
+    # @twilio_client = Twilio::REST::Client.new(twilio_sid, twilio_token)
+    # numbers_to_send_to.each do |number|
+    #   @twilio_client.account.sms.messages.create(
+    #     :from => twilio_phone_number,
+    #     :to => number,
+    #     :body => twilio_body
+    #     )
+    # end
   end
 end
