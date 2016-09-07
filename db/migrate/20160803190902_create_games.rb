afe41b385181640b9a0beed47286302eb6e7ad91
Erica Y <email": null>
2016-09-06 23:00:43 -0700

    Update database to include time

--- db/migrate/20160803190902_create_games.rb
+++ db/migrate/20160803190902_create_games.rb
@@ -4,7 +4,8 @@ class CreateGames < ActiveRecord::Migration[5.0]
       t.integer :home_score
       t.integer :away_score
       t.string :location, null: false
-      t.datetime :date, null: false
+      t.date :date, null: false
+      t.time :time, null: false
       t.timestamps
     end
   end
--- db/schema.rb
+++ db/schema.rb
@@ -19,7 +19,8 @@ ActiveRecord::Schema.define(version: 20160805181650) do
     t.integer  "home_score"
     t.integer  "away_score"
     t.string   "location",   null: false
-    t.datetime "date",       null: false
+    t.date     "date",       null: false
+    t.time     "time",       null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
