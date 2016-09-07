fc0f34b6bae1ea1ba4f9549c6bea8461bd814da5
Erica Y <email": null>
2016-09-06 20:28:12 -0700

    Update ajax for creating new game

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,3 +14,40 @@
 //= require jquery_ujs
 //= require bootstrap-sprockets
 //= require_tree .
+
+
+$('#game-form form').ready(function() {
+checkOptions();
+});
+
+function checkOptions() {
+  $('form select').change(function(event) {
+    event.preventDefault();
+    var clicked_selector = $($(this)[0]).attr('id')
+    var clicked_team = $(this).val()
+    var form_data = $(this).serialize()
+
+    $.ajax({
+      url: $('#game-form form').attr('action') + '/new',
+      method: 'GET',
+      data: form_data
+    })
+
+    .done(function(serverData) {
+        var user_team = serverData[0]
+        var selector_team_options = $('form select').slice(0,2)
+        console.log(clicked_team)
+        for (var opt, i = 0; opt = selector_team_options[i]; i++) {
+          var selector_option = $($(opt)[0]).attr('id')
+          console.log(selector_option)
+          if (clicked_selector == selector_option) {
+            (selector_team_options.splice(i, 1))
+            var other_team = $(selector_team_options[0])
+            other_team.val(user_team)
+          }
+        }
+    })
+  })
+}
+
+
