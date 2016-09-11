87c3099b6b75df0b07e5f18ee02a85f7b59a7b47
Marty McLaughlin <martymclaugh@gmail.com>
2016-09-10 18:05:36 -0700

    add some conditionals in sign up form, if sport is uncapitalized or if sport doesn't exist

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -4,15 +4,20 @@ class PlayersController < ApplicationController
 
   def create
     if params[:player][:coach] == "true"
-      if params[:player][:password] == params[:player][:password_confirmation]
-        player = Player.new(player_params)
-      end
-      if player.save
-        team = Team.create(name: params[:player][:team], league_id: League.find_by(sport: params[:player][:sport]).id)
-        teamplayer = PlayerTeam.create(player_id: player.id, team_id: team.id)
-        session[:player_id] = player.id
-        redirect_to "/leagues/#{team.league_id}/teams/#{team.id}"
+      if params[:player][:sport].capitalize == "Football" || params[:player][:sport].capitalize == "Hockey" || params[:player][:sport].capitalize == "Basketball"
+        if params[:player][:password] == params[:player][:password_confirmation]
+          player = Player.new(player_params)
+        end
+        if player.save
+          team = Team.create(name: params[:player][:team], league_id: League.find_by(sport: params[:player][:sport].capitalize).id)
+          teamplayer = PlayerTeam.create(player_id: player.id, team_id: team.id)
+          session[:player_id] = player.id
+          redirect_to "/leagues/#{team.league_id}/teams/#{team.id}"
+        else
+          redirect_to '/signup'
+        end
       else
+        flash[:notice] = "*Must be 'Football', 'Basketball', or 'Hockey'"
         redirect_to '/signup'
       end
     else
